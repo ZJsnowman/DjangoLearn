72f1233b2a0458f14243364b510db9a03b56d3ec
ZJsnowman <zj2299490@gmail.com>
2018-04-10 11:13:36 +0800

    fix bugs and delete celery

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -115,3 +115,55 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/Users/zhangjun/PycharmProjects/DjangoTest/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'polls': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        }
+    }
+}
--- polls/views.py
+++ polls/views.py
@@ -4,45 +4,50 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.utils import timezone
 
+import logging
 from .models import Question, Choice
+
+logger = logging.getLogger('polls')
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-
+    logger.info(latest_question_list)
     return render(request, 'polls/index.html', context)
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-    def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class ResultView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
+#
+#     def get_queryset(self):
+#         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+#
+#
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
+#
+#     def get_queryset(self):
+#         return Question.objects.filter(pub_date__lte=timezone.now())
+#
+#
+# class ResultView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    logger.info(question)
     return render(request, 'polls/detail.html',
                   {'question': question})
 
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    logger.info(question)
     return render(request, 'polls/results.html', {'question': question})
 
 
