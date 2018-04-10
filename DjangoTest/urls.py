b9a7ad99f3864574a0bce06c3d52a0ad74c5de9b
ZJsnowman <zj2299490@gmail.com>
2018-04-10 11:39:11 +0800

    update to django2.0

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^detail/(?P<article_id>[0-9]+)/$', views.get_detail, name='detail'),
